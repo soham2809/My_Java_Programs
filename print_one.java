b000553a326f5371822b040feb6b4e16dd537fd9
Soham Mukherjee <130496099+soham2809@users.noreply.github.com>
2023-11-27 14:00:26 +0530

    Printing elements present in the array

---
+++ print_one.java
@@ -0,0 +1,12 @@
+//Write a java program to print an entire array
+public class print_one  
+{
+	public static void main (String args[])
+	{
+		int nums[]={12,23,4,55,67,45,78};
+		for (int i=0;i<nums.length;i++)
+		{
+			System.out.println (nums[i]);
+		}
+		System.out.println ();
+}}
