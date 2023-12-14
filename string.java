b6110efdd5f78529ed8e28127aae05470ef319ed
Soham Mukherjee <130496099+soham2809@users.noreply.github.com>
2023-12-15 01:43:31 +0530

    equals() vs "=="
    String comparision through eguals() and "=="

---
+++ string.java
@@ -0,0 +1,10 @@
+public class string
+{
+	public static void main (String[] args)
+	{
+		String s1 ="pwskills";
+		String s2 = new String ("pwskills");
+		System.out.println (s1==s2);
+		System.out.println (s1.equals(s2));
+	}
+}
